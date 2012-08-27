b180844b5d2f921118ccc71d794d86ea59bb7804
Ares <ares@igloonet.cz>
2012-08-27 13:23:55 +0200

    In new redmine we need only one hook
    Also make settings english only - does not work with some localizations

--- lib/work_hours_issue_hook.rb
+++ lib/work_hours_issue_hook.rb
@@ -22,18 +22,18 @@ class WorkHoursIssueHook  < Redmine::Hook::ViewListener
     end
   end
 
-  def view_issues_edit_notes_bottom(context = {})
-    if context[:project].module_enabled?('work_hours')
-      setting = Setting.plugin_redmine_work_hours
-      day = I18n.t('date.day_names', :locale => :en)[Time.now.wday].downcase
-      work_hours_from = Time.parse("#{setting[(day+'_hours_from').to_sym]}:00")
-      work_hours_to = Time.parse("#{setting[(day+'_hours_to').to_sym]}:00")
-      text_to_display = Time.now > work_hours_from && Time.now < work_hours_to ? :text_during_work_hours : :text_not_during_work_hours
-
-      text = textilizable(WorkHoursProjectSetting.first(:conditions => ['project_id = ?', context[:project].id]).send(text_to_display))
-      return "<div class=\"wiki\">#{text}</div>"
-    else
-      return ''
-    end
-  end
+#  def view_issues_edit_notes_bottom(context = {})
+#    if context[:project].module_enabled?('work_hours')
+#      setting = Setting.plugin_redmine_work_hours
+#      day = I18n.t('date.day_names', :locale => :en)[Time.now.wday].downcase
+#      work_hours_from = Time.parse("#{setting[(day+'_hours_from').to_sym]}:00")
+#      work_hours_to = Time.parse("#{setting[(day+'_hours_to').to_sym]}:00")
+#      text_to_display = Time.now > work_hours_from && Time.now < work_hours_to ? :text_during_work_hours : :text_not_during_work_hours
+#
+#      text = textilizable(WorkHoursProjectSetting.first(:conditions => ['project_id = ?', context[:project].id]).send(text_to_display))
+#      return "<div class=\"wiki\">#{text}</div>"
+#    else
+#      return ''
+#    end
+#  end
 end
