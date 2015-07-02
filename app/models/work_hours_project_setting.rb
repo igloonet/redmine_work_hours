3c01b162ecbf6abf49c733ebc723219cca019d10
Michal Gritzbach <gritzbach.michal@gmail.com>
2015-07-02 16:57:38 +0200

    Return work hours settings for today

--- app/models/work_hours_project_setting.rb
+++ app/models/work_hours_project_setting.rb
@@ -12,4 +12,12 @@ class WorkHoursProjectSetting < ActiveRecord::Base
     end
     setting
   end
+
+  def self.settings_for_today
+    setting = Setting.plugin_redmine_work_hours.with_indifferent_access
+    day = I18n.t('date.day_names', :locale => :en)[Time.now.wday].downcase
+
+    require 'ostruct'
+    OpenStruct.new(from: Time.parse("#{setting[(day+'_hours_from').to_sym]}:00"), to: Time.parse("#{setting[(day+'_hours_to').to_sym]}:00"))
+  end
 end
